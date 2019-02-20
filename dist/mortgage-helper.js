e1f0603003903deb068d6f09efe51a52b5802050
Alexandre <alex@wyhn.ca>
2019-02-20 13:27:42 -0500

    Updated dist

--- dist/mortgage-helper.js
+++ dist/mortgage-helper.js
@@ -4,24 +4,56 @@ var _createClass = function () { function defineProperties(target, props) { for 
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-var mortgage = function () {
+var hmortgage = function () {
     'use strict';
 
+    /*
+     * Mortgage Helper
+     * https://github.com/ikarusdev/mortgage-helper
+     *
+     * Copyright (c) 2019 Ikarus
+     * Licensed under the MIT license.
+     */
+
     var HMortgage = function () {
-        function HMortgage(amount, term, rate) {
+        function HMortgage(principal, term, rate) {
             _classCallCheck(this, HMortgage);
 
-            this.validate(amount, term, rate);
-            this.amount = amount;
+            this.validate(principal, term, rate);
+            this.principal = principal;
             this.term = term;
             this.rate = rate;
+            this.initialize();
         }
 
         _createClass(HMortgage, [{
+            key: 'initialize',
+            value: function initialize() {
+                /**
+                 * Your lender likely lists interest rates as an annual figure, 
+                 * so you’ll need to divide by 12, for each month of the year.
+                 * So, if your rate is 5%, then the monthly rate will look like 
+                 * this: 0.05/12 = 0.004167.
+                 */
+                this.mInterestRate = this.rate / 100 / 12;
+
+                /** 
+                 * M = P [ i(1 + i)^n ] / [ (1 + i)^n - 1]
+                 * 
+                 * M = monthly mortgage payment
+                 * P = the principal amount
+                 * i = your monthly interest rate. 
+                 * n = the number of payments over the life of the loan.
+                 *     If you take out a 30-year fixed rate mortgage, this means: n = 30 years x 12 months per year, or 360 payments.
+                 **/
+                var powed = Math.pow(1 + this.mInterestRate, this.term);
+                this.mPayments = HMortgage.round(this.principal * (this.mInterestRate * powed / (powed - 1)));
+            }
+        }, {
             key: 'validate',
-            value: function validate(amount, term, rate) {
-                if (!amount || amount <= 0) {
-                    throw new Error('Bad parameter amount: ' + amount);
+            value: function validate(principal, term, rate) {
+                if (!principal || principal <= 0) {
+                    throw new Error('Bad parameter principal: ' + principal);
                 } else if (!term || term <= 0) {
                     throw new Error('Bad parameter term: ' + term);
                 } else if (!rate || rate <= 0) {
@@ -41,16 +73,16 @@ var mortgage = function () {
                 for (var i = 0; i < this.term; i++) {
                     var next = this.getNextValues(sum);
 
-                    sum += next.capital;
+                    sum += next.principal;
 
                     if (i === this.term - 1) {
-                        sum += next.remain;
-                        next.remain = 0;
+                        sum += next.balance;
+                        next.balance = 0;
                     }
 
-                    amortized.principal.push(next.capital);
-                    amortized.balance.push(next.remain);
+                    amortized.principal.push(next.principal);
                     amortized.interests.push(next.interest);
+                    amortized.balance.push(next.balance);
                 }
 
                 return amortized;
@@ -58,21 +90,24 @@ var mortgage = function () {
         }, {
             key: 'getNextValues',
             value: function getNextValues(sum) {
-                var capital = void 0;
-                var interest = void 0;
-                var installment = void 0;
-                var ratePerMonth = this.rate / (12 * 100);
-
-                var irmPow = Math.pow(1 + ratePerMonth, this.term);
-                installment = HMortgage.round(this.amount * (ratePerMonth * irmPow / (irmPow - 1)));
-                interest = HMortgage.round((this.amount - sum) * ratePerMonth);
-                capital = installment - interest;
+                var interest = HMortgage.round((this.principal - sum) * this.mInterestRate);
+                var principal = this.mPayments - interest;
 
                 return {
-                    capital: capital,
                     interest: interest,
-                    remain: this.amount - sum - capital
+                    principal: principal,
+                    balance: this.principal - sum - principal
                 };
+            }
+        }, {
+            key: 'getMonthlyInterestRate',
+            value: function getMonthlyInterestRate() {
+                return this.mInterestRate;
+            }
+        }, {
+            key: 'getMonthlyPayment',
+            value: function getMonthlyPayment() {
+                return this.mPayments;
             }
         }], [{
             key: 'round',
